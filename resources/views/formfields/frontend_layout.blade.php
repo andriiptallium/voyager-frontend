25e6392e78f22761faf604d39eca44bb3dbea07a
antonshamrov <antonshamrow@gmail.com>
2019-07-27 17:28:42 +0300

    update layouts retrive

--- resources/views/formfields/frontend_layout.blade.php
+++ resources/views/formfields/frontend_layout.blade.php
@@ -1,7 +1,6 @@
 <?php
 $selected_value = (isset($dataTypeContent->{$row->field}) && !is_null(old($row->field, $dataTypeContent->{$row->field}))) ? old($row->field, $dataTypeContent->{$row->field}) : old($row->field);
 
-$layouts = \Pvtl\VoyagerFrontend\Helpers\Layouts::getLayouts('voyager-frontend');
 ?>
 <select class="form-control select2" name="{{ $row->field }}">
     @foreach($layouts as $layout)
